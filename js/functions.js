dc007ee415e3bc1ae9c1fcd07ea9489301cd40a9
unknown <nicolasrios91@hotmail.com>
2021-05-27 18:58:32 -0300

    use of local storage

--- js/functions.js
+++ js/functions.js
@@ -13,7 +13,7 @@ const setLettersList = () => {
 //updates character on page
 const changeCharacter = () => {
   let selectCharacter = $("characters").value;
-  fetchCharacter(selectCharacter);
+  updateCharacter(selectCharacter);
 };
 
 const selectCharacterChange = () => {
@@ -21,23 +21,32 @@ const selectCharacterChange = () => {
   selectCharacter.addEventListener("change", changeCharacter);
 };
 
+const updateCharactersList = () => {
+  let selectLyrics = $("letter");
+  selectLyrics.addEventListener("change", setCharactersList);
+};
+
 //sets list according to letter value
 const setCharactersList = () => {
-  let selectLyrics = $("letter").value;
+  let selectLetters = $("letter").value;
   fetch(
     "https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=" +
-      selectLyrics +
+      selectLetters +
       "&ts=1&apikey=3e8159f9102a91676ab9339a3b0e7135&hash=6126f0add6159a284067d294bbd98c92&limit=100"
   )
     .then((response) => response.json())
     .then((dataResponse) => {
-      let characterList = dataResponse.data.results;
+      localStorage.setItem("data", JSON.stringify(dataResponse.data.results));
       let selectCharacter = $("characters");
 
+      //removes previous options
       for (let i = selectCharacter.length - 1; i >= 1; i--) {
         selectCharacter.remove(i);
       }
 
+      let characterList = JSON.parse(window.localStorage.getItem("data"));
+
+      //sets character list in select box
       characterList.forEach((element) => {
         let option = document.createElement("option");
         option.value = element.name;
@@ -45,11 +54,6 @@ const setCharactersList = () => {
         selectCharacter.appendChild(option);
       });
     });
-};
-
-const updateCharactersList = () => {
-  let selectLyrics = $("letter");
-  selectLyrics.addEventListener("change", setCharactersList);
 };
 
 const $ = (value) => {
@@ -65,6 +69,7 @@ window.addEventListener("load", setLettersList);
 window.addEventListener("load", updateCharactersList);
 window.addEventListener("load", selectCharacterChange);
 
+//
 function fetchCharacter(characterName) {
   return fetch(
     `https://gateway.marvel.com:443/v1/public/characters?name=${characterName}&ts=1&apikey=3e8159f9102a91676ab9339a3b0e7135&hash=6126f0add6159a284067d294bbd98c92`
@@ -72,12 +77,26 @@ function fetchCharacter(characterName) {
     .then((response) => response.json())
     .then((dataResponse) => {
       let character = dataResponse.data.results[0];
-      let imgURL =
-        character.thumbnail.path + "." + character.thumbnail.extension;
-      document.getElementById("profile-image").src = imgURL;
-
-      $("td-character-name").innerHTML = character.name;
-      $("description").innerHTML =
-        character.description || "Sorry! No info available for this character";
+      setCharacter(character);
     });
+}
+
+//updates character data on page according to parameter
+function updateCharacter(characterName) {
+  let character = JSON.parse(window.localStorage.getItem("data"));
+  let mapCharacter = character.filter(function (element) {
+    if (element.name === characterName) {
+      return element; //returns array of 1 element with the selected character
+    }
+  });
+  setCharacter(mapCharacter[0]);
+}
+
+//function to set character on page
+function setCharacter(character) {
+  let imgURL = character.thumbnail.path + "." + character.thumbnail.extension;
+  document.getElementById("profile-image").src = imgURL;
+  $("td-character-name").innerHTML = character.name;
+  $("description").innerHTML =
+    character.description || "Sorry! No info available for this character";
 }
